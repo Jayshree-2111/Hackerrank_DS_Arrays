16f4903bb22e426ca67a5c236c3aa2eac114a72a
Jayshree-2111 <86256037+Jayshree-2111@users.noreply.github.com>
2021-08-27 22:35:03 +0530

    Create 2D_Array_HourGlass_Problem.cpp
    An 6*6 array contains elements in such a manner that 7 elements from each three rows and columns make a hour glass. User need to calculate the sum of elements making hour glass and return the max sum.
    
    Language Used: C++

---
+++ 2D_Array_HourGlass_Problem.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int hourglassSum(vector<vector<int>> arr) {
+    int result=INT_MIN; //initialising the result variable with -infinity 
+    for(int i=0;i<4;i++) //loop for row which will iterate for 3 times for forming 1 hour glass and then incremented for forming next hour glass
+        for(int j=0;j<4;j++) ////loop for column which will iterate for 3 times for forming 1 hour glass and then incremented for forming next hour glass
+        {
+           
+            int sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]; //sum of only those elements which are forming an hour glass 
+            
+            result= std::max(result,sum); //comparing result with sum variable, once the sum is greater than result value then, result variable will be updated.
+                                          
+        }
+        return result; //max result value will be returned back
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < 6; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+ 
